3ed6e22a79bd77dcd80a8d0dd76f780576142a1c
Aarish Khanna <aarishkhanna@gmail.com>
2023-01-23 18:03:04 +0530

    Updated the Project

---
+++ Netflix Clone/Controllers/Core/DownloadsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Aarish Khanna on 11/01/23.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+    
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Downloads"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        view.backgroundColor = .systemBackground
+
+
+    }
+
+  
+    
+    
+
+
+
+
+}
+
+
---
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Aarish Khanna on 23/01/23.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+
+}
